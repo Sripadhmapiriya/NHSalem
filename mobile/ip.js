87643c300aada8c0e5ed70d3d4774b9da437d038
Sripadhmapiriya <sripadhmapiriya12@gmail.com>
2026-08-18 16:42:26 +0530

    chore: remove obsolete test scripts and temporary utility files

--- mobile/ip.js
+++
@@ -1,9 +0,0 @@
-const { networkInterfaces } = require('os');
-const nets = networkInterfaces();
-for (const name of Object.keys(nets)) {
-  for (const net of nets[name]) {
-    if (net.family === 'IPv4' && !net.internal) {
-      console.log(name, net.address);
-    }
-  }
-}
